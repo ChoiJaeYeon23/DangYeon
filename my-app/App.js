f81afbd98732b8ea1dd31f7b27d19682160dbb53
suji_2 <tnwl31091@gmail.com>
2023-11-07 12:32:42 +0900

    screen

--- my-app/App.js
+++ my-app/App.js
@@ -1,16 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator  } from '@react-navigation/stack'
 import couple_connect from './Screen/couple_connect'
 import profileinput from './Screen/profileinput'
 
+const Stack = createStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <couple_connect/>
-      <profileinput/>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="CoupleConnect">
+        <Stack.Screen name="CoupleConnect" component={couple_connect} />
+        <Stack.Screen name="ProfileInput" component={profileinput} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
