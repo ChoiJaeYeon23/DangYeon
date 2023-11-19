16931034936b7ae6d9dcf12d97fe6659562f7302
ChoiJaeYeon23 <wasid4598@gmail.com>
2023-11-19 22:04:44 +0900

    사진등록 후 메타데이터 확인

--- TeamProject/Screens/MainScreen/PictureMap.js
+++ TeamProject/Screens/MainScreen/PictureMap.js
@@ -1,44 +1,53 @@
 import React, { useState } from 'react';
-import { Button, View, Image, Text } from 'react-native';
+import { Button, View, Text, Image, Alert, ScrollView } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-import Exif from 'react-native-exif';
 
 const PictureMap = () => {
   const [imageExif, setImageExif] = useState(null);
+  const [imageUri, setImageUri] = useState(null);
 
   const pickImage = async () => {
     const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    if (permissionResult.granted === false) {
-      alert('갤러리에 접근하기 위한 권한이 필요합니다.');
+    if (!permissionResult.granted) {
+      Alert.alert('권한 필요', '갤러리에 접근하기 위한 권한이 필요합니다.');
       return;
     }
 
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      exif: true,
     });
 
     if (!result.canceled && result.assets) {
-      result.assets.forEach(asset => {
-        Exif.getExif(asset.uri)
-          .then(msg => {
-            console.log(msg);
-            setImageExif(msg.exif);
-          })
-          .catch(msg => console.error('Error getting EXIF data: ', msg));
-      });
+      const selectedAsset = result.assets[0];
+      setImageUri(selectedAsset.uri);
+
+      if (selectedAsset.exif) {
+        setImageExif(selectedAsset.exif);
+        // 콘솔에 EXIF 데이터의 모든 키-값 쌍을 출력
+        console.log('EXIF 데이터:');
+        Object.entries(selectedAsset.exif).forEach(([key, value]) => {
+          console.log(`${key}: ${value}`);
+        });
+      } else {
+        Alert.alert('EXIF 데이터 없음', '선택된 이미지에 EXIF 데이터가 없습니다.');
+        setImageExif(null);
+      }
     }
   };
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Button title="Select Image" onPress={pickImage} />
-      {imageExif && (
-        <View>
-          <Text>Latitude: {imageExif.GPSLatitude}</Text>
-          <Text>Longitude: {imageExif.GPSLongitude}</Text>
-        </View>
-      )}
-    </View>
+    <ScrollView style={{ flex: 1 }}>
+      <View style={{ justifyContent: 'center', alignItems: 'center', marginVertical: 20 }}>
+        <Button title="이미지 선택" onPress={pickImage} />
+        {imageUri && <Image source={{ uri: imageUri }} style={{ width: 200, height: 200, marginVertical: 20 }} />}
+        {imageExif && (
+          Object.entries(imageExif).map(([key, value]) => (
+            <Text key={key}>{`${key}: ${value}`}</Text>
+          ))
+        )}
+      </View>
+    </ScrollView>
   );
 }
 
