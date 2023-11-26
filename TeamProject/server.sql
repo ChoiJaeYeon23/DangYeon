6aff6a617a595cfeac3cead71a296addd2db0184
ChoiJaeYeon23 <wasid4598@gmail.com>
2023-11-26 20:15:42 +0900

    SQL문 작성중

--- TeamProject/server.sql
+++ TeamProject/server.sql
@@ -1,6 +1,14 @@
 DROP DATABASE post;
 CREATE DATABASE post;
-CREATE user
+CREATE userInfo;
+CREATE coupleInfo;
+CREATE chat;
+CREATE bucketList;
+CREATE calendar;
+CREATE picturemap;
+
+USE coupleInfo
+USE userInfo
 USE post;
 
 /* 게시판 테이블 */
@@ -17,13 +25,77 @@ CREATE TABLE postInfo(
 );
 
 /* 유저 정보 테이블 */
+CREATE TABLE userInfo( 
+    username        VARCHAR(20),
+    id              VARCHAR(20),
+    pw              VARCHAR(20),
+    birthday        DATE,
+    connectid       TEXT,
+    bloodtype       VARCHAR(5)
+    userimage       TEXT,
+    coupleid        VARCHAR(20)
+    PRIMARY KEY (id),
+    FOREIGN KEY (couple_id) REFERENCES coupleInfo(couple_id)
+)
 
+/* 커플 정보 테이블 */
+CREATE TABLE coupleInfo(
+    meetdate        DATE,
+    couple_id        VARCHAR(20),
+    PRIMARY KEY (couple_id),
+)
 
+/* 채팅 내역 테이블*/
+CREATE TABLE chat(
+    couple_id                VARCHAR(20)
+    user1_id                 VARCHAR(20),
+    user2_id                 VARCHAR(20),
+    user1_Messagetext        TEXT,
+    user2_Messagetext        TEXT,
+    user1_MessageTime        DATETIME,
+    user2_MessageTime        DATETIME,
+    FOREIGN KEY (user1_id) REFERENCES userInfo(id),
+    FOREIGN KEY (user2_id) REFERENCES userInfo(id),
+    PRIMARY KEY (couple_id) REFERENCES coupleInfo(couple_id)
+)
 
+/* 버킷리스트 테이블 */
+CREATE TABLE bucketList(
+    bucket_id    VARCHAR(20),
+    bucket_text  TEXT,
+    couple_id    VARCHAR(20),
+    PRIMARY KEY (couple_id) REFERENCES coupleInfo(couple_id)
+)
+
+/* 캘린더 테이블 */
+CREATE TABLE calendar(
+    schedule_id     VARCHAR(20),
+    schedule_text   TEXT,
+    schedule_date   DATE,
+    couple_id    VARCHAR(20),
+    PRIMARY KEY (couple_id) REFERENCES coupleInfo(couple_id)
+)
+
+/* 사진지도 테이블 */
+CREATE TABLE picturemap(
+    image_id    TEXT,
+    image_date  DATE,
+    couple_id    VARCHAR(20),
+    PRIMARY KEY (couple_id) REFERENCES coupleInfo(couple_id)
+)
+
+/* 랭킹 테이블 */
+-- CREATE TABLE calendar(
+    -- couple_id    VARCHAR(20),
+    -- PRIMARY KEY (couple_id) REFERENCES coupleInfo(couple_id)
+-- )
+/* 출석체크 테이블 */
+-- CREATE TABLE calendar(
+    -- couple_id    VARCHAR(20),
+    -- PRIMARY KEY (couple_id) REFERENCES coupleInfo(couple_id)
+-- )
 
 set SQL_SAFE_UPDATES = 0;
-
-
 
 
 AlTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'dlavnf1290@!';
