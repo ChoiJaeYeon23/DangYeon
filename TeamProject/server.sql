f17172e6703d2d0d35d15cf9eddf8c51e7f3710d
이호민2 <bup0520f@gmail.com>
2023-11-26 21:15:54 +0900

    DB 구축 (70%완성)

--- TeamProject/server.sql
+++ TeamProject/server.sql
@@ -1,102 +1,74 @@
-DROP DATABASE post;
-CREATE DATABASE post;
-CREATE userInfo;
-CREATE coupleInfo;
-CREATE chat;
-CREATE bucketList;
-CREATE calendar;
-CREATE picturemap;
+DROP DATABASE dang;
+CREATE DATABASE dang;
+USE dang;
 
-USE coupleInfo
-USE userInfo
-USE post;
-
-/* 게시판 테이블 */
-CREATE TABLE postInfo(
-    postNum        INTEGER PRIMARY KEY auto_increment,
-    author         VARCHAR(20),
-    authorIcon     text,
-    email          text,
-    title         text,
-    content         LONGTEXT,
-    img            text,
-    DATE         text,
-    hit            INTEGER
+-- 커플 정보 테이블
+CREATE TABLE coupleInfo(
+    couple_id VARCHAR(20) PRIMARY KEY,
+    meet_date DATE
 );
 
-/* 유저 정보 테이블 */
+-- 유저 정보 테이블
 CREATE TABLE userInfo( 
-    username        VARCHAR(20),
-    id              VARCHAR(20),
-    pw              VARCHAR(20),
-    birthday        DATE,
-    connectid       TEXT,
-    bloodtype       VARCHAR(5)
-    userimage       TEXT,
-    coupleid        VARCHAR(20)
-    PRIMARY KEY (id),
+    id VARCHAR(20) PRIMARY KEY,
+    username VARCHAR(20),
+    pw VARCHAR(20),
+    birthday DATE,
+    connect_id TEXT,
+    blood_type VARCHAR(5),
+    user_image TEXT,
+    couple_id VARCHAR(20),
     FOREIGN KEY (couple_id) REFERENCES coupleInfo(couple_id)
-)
+);
 
-/* 커플 정보 테이블 */
-CREATE TABLE coupleInfo(
-    meetdate        DATE,
-    couple_id        VARCHAR(20),
-    PRIMARY KEY (couple_id),
-)
+-- 게시판 정보 테이블
+CREATE TABLE postInfo(
+    post_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    postdate DATE,
+    title VARCHAR(255),
+    content TEXT,
+    img TEXT,
+    couple_id VARCHAR(20),
+    FOREIGN KEY (couple_id) REFERENCES coupleInfo(couple_id)
+);
 
-/* 채팅 내역 테이블*/
+-- 채팅 내역 테이블
 CREATE TABLE chat(
-    couple_id                VARCHAR(20)
-    user1_id                 VARCHAR(20),
-    user2_id                 VARCHAR(20),
-    user1_Messagetext        TEXT,
-    user2_Messagetext        TEXT,
-    user1_MessageTime        DATETIME,
-    user2_MessageTime        DATETIME,
-    FOREIGN KEY (user1_id) REFERENCES userInfo(id),
-    FOREIGN KEY (user2_id) REFERENCES userInfo(id),
-    PRIMARY KEY (couple_id) REFERENCES coupleInfo(couple_id)
-)
+    chat_id VARCHAR(20) PRIMARY KEY,
+    couple_id VARCHAR(20),
+    user1_Message_text TEXT,
+    user2_Message_text TEXT,
+    user1_MessageTime DATETIME,
+    user2_MessageTime DATETIME,
+    FOREIGN KEY (couple_id) REFERENCES coupleInfo(couple_id)
+);
 
-/* 버킷리스트 테이블 */
+-- 버킷리스트 테이블
 CREATE TABLE bucketList(
-    bucket_id    VARCHAR(20),
-    bucket_text  TEXT,
-    couple_id    VARCHAR(20),
-    PRIMARY KEY (couple_id) REFERENCES coupleInfo(couple_id)
-)
+    bucket_id VARCHAR(20) PRIMARY KEY,
+    bucket_text TEXT,
+    couple_id VARCHAR(20),
+    FOREIGN KEY (couple_id) REFERENCES coupleInfo(couple_id)
+);
 
-/* 캘린더 테이블 */
+-- 캘린더 테이블
 CREATE TABLE calendar(
-    schedule_id     VARCHAR(20),
-    schedule_text   TEXT,
-    schedule_date   DATE,
-    couple_id    VARCHAR(20),
-    PRIMARY KEY (couple_id) REFERENCES coupleInfo(couple_id)
-)
+    schedule_id VARCHAR(20) PRIMARY KEY,
+    schedule_text TEXT,
+    schedule_date DATE,
+    couple_id VARCHAR(20),
+    FOREIGN KEY (couple_id) REFERENCES coupleInfo(couple_id)
+);
 
-/* 사진지도 테이블 */
-CREATE TABLE picturemap(
-    image_id    TEXT,
-    image_date  DATE,
-    couple_id    VARCHAR(20),
-    PRIMARY KEY (couple_id) REFERENCES coupleInfo(couple_id)
-)
+-- 사진지도 테이블
+CREATE TABLE picture_map(
+    image_id VARCHAR(20) PRIMARY KEY,
+    image_date DATE,
+    couple_id VARCHAR(20),
+    FOREIGN KEY (couple_id) REFERENCES coupleInfo(couple_id)
+);
 
-/* 랭킹 테이블 */
--- CREATE TABLE calendar(
-    -- couple_id    VARCHAR(20),
-    -- PRIMARY KEY (couple_id) REFERENCES coupleInfo(couple_id)
--- )
-/* 출석체크 테이블 */
--- CREATE TABLE calendar(
-    -- couple_id    VARCHAR(20),
-    -- PRIMARY KEY (couple_id) REFERENCES coupleInfo(couple_id)
--- )
-
-set SQL_SAFE_UPDATES = 0;
-
+SET SQL_SAFE_UPDATES = 0;
 
 AlTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'dlavnf1290@!';
 FLUSH privileges;
