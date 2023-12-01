da1acdad16938bbfb580fcab13c06d7a1d30f110
suji_2 <tnwl31091@gmail.com>
2023-12-01 13:33:05 +0900

    현재위치

--- TeamProject/Screens/EtcScreen/PedometerScreen.js
+++ TeamProject/Screens/EtcScreen/PedometerScreen.js
@@ -1,16 +1,33 @@
 import { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
 import { Pedometer } from 'expo-sensors';
+import * as Location from 'expo-location';
 
 const PedometerScreen = () => {
-  // 만보기가 사용 가능한지 여부를 저장하는 상태
-  const [isPedometerAvailable, setIsPedometerAvailable] = useState('checking');
-  // 지난 24시간 동안의 걸음 수를 저장하는 상태
-  const [pastStepCount, setPastStepCount] = useState(0);
-  // 현재 걸음 수를 저장하는 상태
-  const [currentStepCount, setCurrentStepCount] = useState(0);
+  const [isPedometerAvailable, setIsPedometerAvailable] = useState('checking');// 만보기가 사용 가능한지 여부를 저장하는 상태
+  const [pastStepCount, setPastStepCount] = useState(0); // 지난 24시간 동안의 걸음 수를 저장하는 상태
+  const [currentStepCount, setCurrentStepCount] = useState(0);// 현재 걸음 수를 저장하는 상태
+  const [location, setLocation] = useState(null); // 현재 위치를 저장하는 상태
+  const [errorMsg, setErrorMsg] = useState(null); // 오류 메시지를 저장하는 상태
 
   let subscription = null; // subscription을 외부 변수로 선언
+
+  // 위치 정보를 가져오는 함수
+  const getLocationAsync = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      Alert.alert("위치 권한 필요", "앱이 위치 정보에 접근하려면 위치 권한이 필요합니다.");
+      return;
+    }
+
+    let currentLocation = await Location.getCurrentPositionAsync({});
+    setLocation(currentLocation);
+
+    // 가상의 조건으로 근접 여부 확인 (실제 구현 시 수정 필요)
+    if (currentLocation.coords.latitude < 10 && currentLocation.coords.longitude < 10) {
+      Alert.alert("알람", "사랑의 걸음, 함께 시작해볼까요?");
+    }
+  };
 
   useEffect(() => {
     // 만보기 기능 사용 가능 여부 확인
@@ -34,6 +51,8 @@ const PedometerScreen = () => {
       }
     });
 
+    getLocationAsync(); // 위치 정보 가져오기
+
     // 구독 해제
     return () => {
       if (subscription) {
@@ -47,6 +66,8 @@ const PedometerScreen = () => {
       <Text>만보기 기능 사용 가능 여부 : {isPedometerAvailable}</Text>
       <Text>지난 24시간 동안 걸음 수 : {pastStepCount}</Text>
       <Text>현재 걸음 수 : {currentStepCount}</Text>
+      {errorMsg ? <Text>오류: {errorMsg}</Text> : null}
+      {location ? <Text>현재 위치: {JSON.stringify(location)}</Text> : null}
     </View>
   );
 }
