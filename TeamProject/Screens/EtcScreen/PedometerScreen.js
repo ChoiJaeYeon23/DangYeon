6be96b80a770a9e6ff0cb9ee5198bc57d0020dcd
suji_2 <tnwl31091@gmail.com>
2023-12-05 00:31:17 +0900

    만보기 상대 위치 해야함

--- TeamProject/Screens/EtcScreen/PedometerScreen.js
+++ TeamProject/Screens/EtcScreen/PedometerScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Alert } from 'react-native';
+import { StyleSheet, Text, View, Alert, Button } from 'react-native';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Pedometer } from 'expo-sensors';
 import * as Location from 'expo-location';
@@ -10,43 +10,72 @@ const PedometerScreen = () => {
   const [currentStepCount, setCurrentStepCount] = useState(0);// 현재 걸음 수를 저장하는 상태
   const [location, setLocation] = useState(null); // 현재 위치를 저장하는 상태
   const [errorMsg, setErrorMsg] = useState(null); // 오류 메시지를 저장하는 상태
-  const [candies, setCandies] = useState(0); // 획득한 캔디 수
+  const [isTogether, setIsTogether] = useState(false); // 커플이 함께 있는지 여부
+  const [candies, setCandies] = useState({ 1000: 0, 5000: 0, 10000: 0 }); // 캔디 보상 상태
 
-  let subscription = null; // subscription을 외부 변수로 선언
+  let subscription = null; // 걸음 수 감시 기능을 외부 변수로 선언
 
-  // 위치 정보를 가져오는 함수
-  const getLocationAsync = async () => {
-    let { status } = await Location.requestForegroundPermissionsAsync();
-    if (status !== 'granted') {
-      Alert.alert("위치 권한 필요", "앱이 위치 정보에 접근하려면 위치 권한이 필요합니다.");
-      setErrorMsg('위치 권한이 거부되었습니다.');
-      return;
-    }
+  // 현재 위치 업데이트 및 저장 함수
+  const updateAndStoreUserLocation = async () => {
+    try {
+      let currentLocation = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.High });
 
-    let currentLocation = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.High });
-    setLocation(currentLocation); // 현재 위치 상태 업데이트
+      const { latitude, longitude } = currentLocation.coords;
 
-    // 여기에 서버로부터 상대방의 위치를 가져오는 로직을 구현하시면 됩니다.
-    // 예시로, 하드코딩된 상대방 위치를 사용합니다.
-    let partnerLocation = { /* 상대방 위치 데이터 */ };
+      // 위도와 경도를 AsyncStorage에 저장합니다.
+      await AsyncStorage.setItem('@userLocation', JSON.stringify({ latitude, longitude }));
 
-    // 두 위치가 일정 거리 이내인지 확인 (예: 100미터 이내)
-    // partnerLocation이 유효한지 확인
-    if (partnerLocation && partnerLocation.coords && currentLocation && currentLocation.coords) {
-      if (isWithinDistance(currentLocation, partnerLocation, 100)) {
-        Alert.alert("알람", "사랑의 걸음, 함께 시작해볼까요?");
-      }
+      // 저장된 위치와 현재 위치를 비교합니다.
+      await compareWithStoredLocation(currentLocation.coords);
+    } catch (error) {
+      console.error('위치 업데이트 중 오류 발생:', error);
     }
   };
+  useEffect(() => {
+    updateAndStoreUserLocation(); // 초기 로드 시 위치 업데이트
+    trackLocation(); // 위치 추적 시작
+  }, []);
 
-  // 두 위치가 주어진 거리 이내에 있는지 확인하는 함수
-  const isWithinDistance = (loc1, loc2, maxDistance) => {
-    let distance = getDistanceBetweenLocations(loc1.coords, loc2.coords);
-    return distance <= maxDistance;
+  const trackLocation = async () => { // 사용자 위치 실시간으로 추적 함수(100m 변할 때마다)
+    await Location.requestForegroundPermissionsAsync();
+
+    Location.watchPositionAsync(
+      {
+        accuracy: Location.Accuracy.BestForNavigation,
+        distanceInterval: 100, // 100미터마다 위치 업데이트
+      },
+      async (newLocation) => {
+        let { latitude, longitude } = newLocation.coords;
+
+        // 새로운 위치 데이터를 AsyncStorage에 저장합니다.
+        try {
+          await AsyncStorage.setItem('@userLocation', JSON.stringify({ latitude, longitude }));
+          console.log("새 위치 저장됨:", latitude, longitude);
+        } catch (error) {
+          console.error('위치 저장 중 오류 발생:', error);
+        }
+      }
+    );
+  };
+
+  // 저장된 위치와 비교하는 함수
+  const compareWithStoredLocation = async (currentCoords) => {
+    try {
+      const storedLocationString = await AsyncStorage.getItem('@userLocation');
+      if (storedLocationString !== null) {
+        const storedLocation = JSON.parse(storedLocationString);
+        const distance = getDistanceBetweenLocations(storedLocation, currentCoords);
+        if (distance <= 20) { // 20미터 이내로 설정
+          Alert.alert("두 사람이 가까이 있습니다. 만보기 시작하겠습니다!");
+          setIsTogether(true); // 만보기 시작
+        }
+      }
+    } catch (error) {
+      console.error('데이터 비교 중 오류 발생:', error);
+    }
   };
 
   // 각 위치의 위도와 경도 값으로 이 공식을 사용하여 어떤 지점에서든 두 위치 사이의 거리 계산 가능
-  // 위도 경도 달라져도 동일하게 공식 적용
   // 두 위치 사이의 거리 계산 함수 Haversine 공식 사용
   const getDistanceBetweenLocations = (coords1, coords2) => {
     const toRadians = (degree) => degree * (Math.PI / 180);
@@ -65,47 +94,29 @@ const PedometerScreen = () => {
     return earthRadiusKm * c;
   };
 
-  // 상대방 위치 데이터를 서버로부터 가져오는 함수 (가상 구현)
-  const getPartnerLocationFromServer = async () => {
-    // 서버로부터 상대방 위치 데이터를 받아오는 로직 구현
-    try {
-      // 서버 URL 및 필요한 인증 정보 (예: 사용자 토큰)
-      const url = "";
-      const response = await fetch(url, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          // 인증 토큰이 필요한 경우 아래 헤더에 추가
-          // "Authorization": "Bearer YOUR_TOKEN"
-        }
-      });
+  // 캔디 보상 함수
+  const claimCandy = (steps) => {
+    let newCandies = { ...candies };
+    newCandies[steps] = newCandies[steps] + 1;
 
-      if (!response.ok) {
-        throw new Error('서버로부터 응답을 받는 데 실패했습니다.');
-      }
-      const data = await response.json();
-      return data; // 서버로부터 받은 위치 데이터
-    } catch (error) {
-      console.error("서버 요청 실패:", error);
-      return null; // 에러 발생 시 null 반환
-    }
+    setCandies(newCandies);
+    Alert.alert("축하합니다!", `캔디 ${newCandies[steps]}개를 획득했습니다!`);
   };
+
+  useEffect(() => {
+    checkForCandyReward(currentStepCount);
+  }, [currentStepCount]);
 
   // 캔디 보상을 확인하는 함수
   const checkForCandyReward = async (steps) => {
     try {
-      const candyMilestones = [1000, 5000, 10000]; // 캔디 보상 단계
-      let earnedCandies = 0;
+      const candyMilestones = [10, 5000, 10000]; // 캔디 보상 단계
+      let earnedCandies = candies; // 현재 캔디 상태를 로컬 변수에 복사
 
       for (const milestone of candyMilestones) {
-        if (steps >= milestone) {
-          earnedCandies += 1;
+        if (steps >= milestone && earnedCandies * 10 < steps) {
+          earnedCandies += 1; // 캔디 보상 추가
         }
-      }
-
-      const storedCandies = await AsyncStorage.getItem('@candies');
-      if (storedCandies !== null) {
-        earnedCandies += parseInt(storedCandies);
       }
 
       if (earnedCandies > candies) {
@@ -118,49 +129,69 @@ const PedometerScreen = () => {
     }
   };
 
-  const loadData = async () => { // 현재 걸음 수 불러오기
-    try {
-      const storedSteps = await AsyncStorage.getItem('@currentStepCount');
-      const storedCandies = await AsyncStorage.getItem('@candies');
-
-      if (storedSteps !== null) {
-        setCurrentStepCount(parseInt(storedSteps, 10));
-      }
-      if (storedCandies !== null) {
-        setCandies(parseInt(storedCandies, 10));
-      }
-    } catch (error) {
-      console.error('데이터 불러오기 중 오류 발생:', error);
-    }
-  };
+  const RewardBox = ({ steps, candies, candyReward, onClaim }) => (
+    <View style={styles.rewardBox}>
+      <Text style={styles.text}>{steps} 걸음</Text>
+      <Text style={styles.text}>캔디 {candies[steps]}개</Text>
+      <Button title={`${candyReward} 캔디 받기!`} onPress={() => onClaim(steps)} />
+    </View>
+  );
 
   useEffect(() => {
-    loadData(); // 앱 시작 시 현재 걸음 수 불러오기
-    // 만보기 기능 사용 가능 여부 확인
-    Pedometer.isAvailableAsync().then(isAvailable => {
+    const loadData = async () => { //현재 걸음 수 불러오기
+      try {
+        const storedSteps = await AsyncStorage.getItem('@currentStepCount');
+        if (storedSteps !== null) {
+          setCurrentStepCount(parseInt(storedSteps, 10));
+        }
+      } catch (error) {
+        console.error('데이터 불러오기 중 오류 발생:', error);
+      }
+    };
+
+    loadData();
+  }, []);
+
+  useEffect(() => {
+    // 커플이 함께 있을 때만 걸음 수 감시
+    if (isTogether) {
+      subscription = Pedometer.watchStepCount(result => {
+        // 현재 걸음 수에 실시간 걸음 수를 더함
+        setCurrentStepCount(prevCount => prevCount + result.steps);
+      });
+    } else {
+      if (subscription) {
+        subscription.remove();
+      }
+    }
+
+    return () => {
+      if (subscription) {
+        subscription.remove();
+      }
+    };
+  }, [isTogether]);
+
+  useEffect(() => {
+    Pedometer.isAvailableAsync().then(isAvailable => { //만보기 사용 가능한지 여부 판단
       setIsPedometerAvailable(String(isAvailable));
 
       if (isAvailable) {
+        // 지난 24시간 동안의 걸음 수 가져오기
         const end = new Date();
         const start = new Date();
         start.setDate(end.getDate() - 1);
 
-        // 지난 24시간 동안의 걸음 수 가져오기
         Pedometer.getStepCountAsync(start, end).then(pastStepCountResult => {
           setPastStepCount(pastStepCountResult.steps);
         });
 
         // 실시간 걸음 수 감시 시작
         subscription = Pedometer.watchStepCount(result => {
-          setCurrentStepCount(result.steps);
-          checkForCandyReward(result.steps).then(() => {
-            console.log(`현재 걸음 수: ${result.steps}, 획득한 캔디 수: ${candies}`);
-          });
+          setCurrentStepCount(prevCount => prevCount + result.steps);
         });
       }
     });
-
-    getLocationAsync(); // 위치 정보 가져오기
 
     // 구독 해제
     return () => {
@@ -174,23 +205,28 @@ const PedometerScreen = () => {
     const storeData = async () => {
       try {
         await AsyncStorage.setItem('@currentStepCount', currentStepCount.toString());
-        await AsyncStorage.setItem('@candies', candies.toString());
       } catch (error) {
         console.error('데이터 저장 중 오류 발생:', error);
       }
     };
-    storeData(); // 현재 걸음 수와 캔디 수 저장
-  }, [currentStepCount, candies]);
+    storeData(); // 현재 걸음 수 저장
+  }, [currentStepCount]);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.titleText}>만보기 정보</Text>
+      <Text style={styles.titleText}>커플 만보기</Text>
+      <View style={styles.separator}></View>
       <Text style={styles.text}>만보기 기능 사용 가능 여부 : {isPedometerAvailable}</Text>
       <Text style={styles.text}>지난 24시간 동안 걸음 수 : {pastStepCount}</Text>
       <Text style={styles.text}>현재 걸음 수 : {currentStepCount}</Text>
-      <Text style={styles.text}>획득한 캔디 수: {candies}</Text>
+      <Button title="위치 업데이트" onPress={updateAndStoreUserLocation} />
+      {location ? <Text>현재 위치: {JSON.stringify(location)}</Text> : null}
       {errorMsg ? <Text style={styles.errorText}>오류: {errorMsg}</Text> : null}
-      <View style={styles.separator}></View>
+      <View style={styles.rewardContainer}>
+        <RewardBox steps={1000} candies={candies} candyReward={1} onClaim={claimCandy} />
+        <RewardBox steps={5000} candies={candies} candyReward={5} onClaim={claimCandy} />
+        <RewardBox steps={10000} candies={candies} candyReward={10} onClaim={claimCandy} />
+      </View>
     </View>
   );
 }
@@ -209,6 +245,17 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "bold",
     marginBottom: 20,
+  },
+  rewardContainer: {
+    flexDirection: 'row',
+  },
+  rewardBox: {
+    margin: 5,
+    padding: 14,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 10,
+    alignItems: 'center',
   },
   text: {
     fontSize: 18,
