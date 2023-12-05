a0a76a1df867dd11bb4b8a7bdbfb372a6d2e7b87
이호민2 <bup0520f@gmail.com>
2023-12-06 03:25:34 +0900

    하늘이한테 받은 게시글 코드

--- TeamProject/Screens/BoardScreen/Gesigeul.js
+++ TeamProject/Screens/BoardScreen/Gesigeul.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
   View,
   TextInput,
@@ -9,16 +9,16 @@ import {
   ScrollView,
   Alert,
   Modal,
-  Keyboard
-} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import * as ImagePicker from 'expo-image-picker';
-import { useNavigation } from '@react-navigation/native';
+  Keyboard,
+} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import * as ImagePicker from "expo-image-picker";
+import { useNavigation } from "@react-navigation/native";
 
 const Gesigeul = ({ route }) => {
-  const [title, setTitle] = useState(''); // 제목 입력 상태
-  const [text, setText] = useState(''); // 내용 입력 상태
-  const [imageSources, setImageSources] = useState([]); // 이미지 소스 배열 
+  const [title, setTitle] = useState(""); // 제목 입력 상태
+  const [text, setText] = useState(""); // 내용 입력 상태
+  const [imageSources, setImageSources] = useState([]); // 이미지 소스 배열
   const [modalVisible, setModalVisible] = useState(false); // 모달 표시 상태
   const [postIdToEdit, setPostIdToEdit] = useState(null); // 수정할 게시물의 ID 저장
   const navigation = useNavigation();
@@ -26,9 +26,10 @@ const Gesigeul = ({ route }) => {
 
   // 이미지 선택
   const pickImage = async () => {
-    const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    const permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
     if (!permissionResult.granted) {
-      Alert.alert('권한 필요', '갤러리에 접근하기 위한 권한이 필요합니다.');
+      Alert.alert("권한 필요", "갤러리에 접근하기 위한 권한이 필요합니다.");
       return;
     }
 
@@ -38,19 +39,19 @@ const Gesigeul = ({ route }) => {
     });
 
     if (!result.canceled && result.assets) {
-      setImageSources(result.assets.map(asset => ({ uri: asset.uri })));
+      setImageSources(result.assets.map((asset) => ({ uri: asset.uri })));
     }
   };
 
   // 저장 버튼 클릭 시 호출되는 함수
   const handleSavePress = () => {
-    console.log('제목 저장: ', title);
-    console.log('내용 저장: ', text);
+    console.log("제목 저장: ", title);
+    console.log("내용 저장: ", text);
 
     const postData = {
       title: title, // 제목 추가
       text: text,
-      images: imageSources.map(source => source.uri)
+      images: imageSources.map((source) => source.uri),
     };
 
     if (postIdToEdit !== null) {
@@ -59,10 +60,10 @@ const Gesigeul = ({ route }) => {
         id: postIdToEdit,
         ...postData,
       };
-      navigation.navigate('Board', { editedData: editedPost });
+      navigation.navigate("Board", { editedData: editedPost });
     } else {
       // 새로운 게시물 데이터를 Board 컴포넌트로 전달
-      navigation.navigate('Board', { postData: postData });
+      navigation.navigate("Board", { postData: postData });
     }
   };
 
@@ -79,7 +80,7 @@ const Gesigeul = ({ route }) => {
       const { title, text, images, id } = route.params.editingPost;
       setTitle(title);
       setText(text);
-      setImageSources(images.map(uri => ({ uri })));
+      setImageSources(images.map((uri) => ({ uri })));
       setPostIdToEdit(id);
     }
   }, [route.params?.editingPost]);
@@ -112,13 +113,9 @@ const Gesigeul = ({ route }) => {
             multiline
             autoFocus
           />
-        ) : (
-          text.length === 0 ? (
-            <Text style={styles.input}>
-              내용을 입력하세요.
-            </Text>
-          ) : null
-        )}
+        ) : text.length === 0 ? (
+          <Text style={styles.input}>내용을 입력하세요.</Text>
+        ) : null}
       </TouchableOpacity>
       <ScrollView horizontal style={styles.imageScroll}>
         {imageSources.map((source, index) => (
@@ -145,8 +142,8 @@ const Gesigeul = ({ route }) => {
               style={styles.modalInput}
               onChangeText={setText}
               value={text}
-              placeholder="내용을 입력하세요."  // 플레이스홀더 텍스트 추가
-              placeholderTextColor="#989292"  // 플레이스홀더 텍스트 색상 설정
+              placeholder="내용을 입력하세요." // 플레이스홀더 텍스트 추가
+              placeholderTextColor="#989292" // 플레이스홀더 텍스트 색상 설정
               multiline
               autoFocus
             />
@@ -166,40 +163,40 @@ const Gesigeul = ({ route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFF9F9',
+    backgroundColor: "#FFF9F9",
   },
   header: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
+    flexDirection: "row",
+    justifyContent: "flex-end",
     padding: 10,
   },
   doneButton: {
     borderWidth: 1,
-    borderColor: 'black',
+    borderColor: "black",
     padding: 5,
     borderRadius: 10,
-    backgroundColor: '#FDE0E0',
+    backgroundColor: "#FDE0E0",
   },
   doneText: {
     fontSize: 12,
-    color: 'black',
+    color: "black",
   },
   titleInput: {
     fontSize: 18, // 제목 폰트 크기
-    fontWeight: 'bold', // 제목 폰트 굵기
-    color: 'black', // 제목 폰트 색상
+    fontWeight: "bold", // 제목 폰트 굵기
+    color: "black", // 제목 폰트 색상
     margin: 10, // 외부 여백
     borderBottomWidth: 1, // 하단 테두리 추가
-    borderColor: 'gray', // 테두리 색상
+    borderColor: "gray", // 테두리 색상
   },
   inputContainer: {
-    borderColor: 'gray',
+    borderColor: "gray",
     padding: 10,
     marginTop: 10,
   },
   input: {
     fontSize: 16,
-    color: '#989292',
+    color: "#989292",
   },
   imageScroll: {
     height: 210,
@@ -210,43 +207,43 @@ const styles = StyleSheet.create({
     marginRight: 10,
   },
   footer: {
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
+    flexDirection: "row",
+    justifyContent: "flex-start",
     padding: 10,
   },
   cameraButton: {
-    alignItems: 'center',
+    alignItems: "center",
     padding: 10,
   },
   centeredView: {
     flex: 1,
-    justifyContent: 'flex-start', // 화면 상단에 정렬
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.4)', // 배경을 어둡게 조정
+    justifyContent: "flex-start", // 화면 상단에 정렬
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.4)", // 배경을 어둡게 조정
   },
   inputBox: {
     marginTop: 98, // "게시판" 텍스트 아래에 위치하도록 marginTop 조정
-    width: '100%', // 전체 너비
-    backgroundColor: 'white', // 배경색 흰색
-    alignItems: 'center',
+    width: "100%", // 전체 너비
+    backgroundColor: "white", // 배경색 흰색
+    alignItems: "center",
     padding: 20,
   },
   modalInput: {
-    width: '100%', // 입력 박스 너비
+    width: "100%", // 입력 박스 너비
     minHeight: 200, // 입력 박스 최소 높이
     padding: 10, // 내부 패딩
   },
   button: {
-    backgroundColor: '#2196F3',
+    backgroundColor: "#2196F3",
     borderRadius: 20,
     padding: 10,
     elevation: 2,
     marginTop: 10,
   },
   buttonText: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
 
