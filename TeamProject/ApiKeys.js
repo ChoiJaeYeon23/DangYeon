7caf634f30a28ea8d34724a7b8dad9d835308281
이호민2 <bup0520f@gmail.com>
2023-12-08 21:45:11 +0900

    no message

---
+++ TeamProject/ApiKeys.js
@@ -0,0 +1,7 @@
+const ApiKeys = {
+  WEATHER_API_KEY: "9edb951af86953242ae7a71e5c342ad2",
+  GEOCODING_API_KEY: "AIzaSyAUoOgEdqAJjl2MbnqQiztR-8Et2_vFQMA",
+  OPENAI_API_KEY: "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",
+};
+
+export default ApiKeys;
